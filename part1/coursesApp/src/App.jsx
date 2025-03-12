6017eee12ddbe78f710fffa16555984c5310c087
Md Shahzeb <mdshahzeb@live.in>
2025-03-13 02:59:26 +0530

    Add exercises 1.1 and 1,2

--- part1/coursesApp/src/App.jsx
+++ part1/coursesApp/src/App.jsx
@@ -13,18 +13,36 @@ const App = () => {
     )
   }
 
+  const Part1 = (props) => {
+    return (
+      <p>
+        {props.part1} {props.exercises1}
+      </p>
+    )
+  }
+
+  const Part2 = (props) => {
+    return (
+      <p>
+        {props.part2} {props.exercises2}
+      </p>
+    )
+  }
+  
+  const Part3 = (props) => {
+    return (
+      <p>
+      {props.part3} {props.exercises3}
+      </p>
+    )
+  }
+
   const Content = (props) => {
     return (
       <div>
-        <p>
-        {props.part1} {props.exercises1}
-        </p>
-        <p>
-        {props.part2} {props.exercises2}
-        </p>
-        <p>
-        {props.part3} {props.exercises3}
-        </p>
+        <Part1 part1= {part1} exercises1 = {exercises1}/>
+        <Part2 part2= {part2} exercises2 = {exercises2}/>
+        <Part3 part3= {part3} exercises3 = {exercises3}/>
       </div>
     )
   }
@@ -51,9 +69,7 @@ const App = () => {
       </p>
       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p> */}
       <Header course = {course}/>
-      <Content part1 = {part1} exercises1 = {exercises1}/>
-      <Content part2 = {part2} exercises2 = {exercises2}/>
-      <Content part3 = {part3} exercises3 = {exercises3}/>
+      <Content />
       <Total/>
     </div>
   )
